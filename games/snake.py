74524b1a990b436c11506cbc14e3faf7475a71ac
hussain1897 <ali-hussain10t@hotmail.co.uk>
2022-05-04 15:20:22 +0100

    snake can eat snack and gets longer

--- games/snake.py
+++ games/snake.py
@@ -1,4 +1,5 @@
-import pygame, math, random
+import pygame, random
+import math
 import tkinter as tk
 from tkinter import messagebox
 
@@ -82,8 +83,25 @@ class snake(object):
 
     def reset(self, pos):
         pass
-    def add_cube(self):
-        pass
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+        
+        # We need to know which side of the snake to add the cube to.
+        # So we check what direction we are currently moving in to determine if we
+        # need to add the cube to the left, right, above or below.
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+        
+        # We then set the cubes direction to the direction of the snake.
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
   
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -104,25 +122,36 @@ def draw_grid(w, rows , surface):
         pygame.draw.line(surface, (255,255,255), (0,y),(w,y))  #keep x at 0, change y horizontal 
 
 def redraw_window(surface):
-    global rows, width, s
+    global rows, width, s, snack
     surface.fill((0,0,0))
     s.draw(surface)
+    snack.draw(surface)
     draw_grid(width, rows, surface)
     pygame.display.update()
 
 
-def random_snack(rows, items):
-    pass
+def random_snack(rows, item):
+    positions = item.body
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) >0:
+            continue
+        else:
+            break
+        
+    return (x,y)
 
 def message_box(subject, content):
     pass
 
 def main():
-    global width, rows, s
+    global width, rows, s, snack
     width = 500
     rows = 20
     win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
+    snack = cube(random_snack(rows,s), colour = (0,255,0))
     flag = True
 
     clock = pygame.time.Clock()
@@ -131,5 +160,8 @@ def main():
         pygame.time.delay(50)
         clock.tick(10)
         s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            snack = cube(random_snack(rows,s), colour = (0,255,0))
         redraw_window(win)
 main()
