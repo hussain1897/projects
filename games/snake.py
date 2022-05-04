655e507294f0a3674f2e7e19e5674867cf936a17
hussain1897 <ali-hussain10t@hotmail.co.uk>
2022-05-04 14:11:58 +0100

    fixed bugs game now running

--- games/snake.py
+++ games/snake.py
@@ -1,18 +1,36 @@
-import pygame, sys, math, random
+import pygame, math, random
 import tkinter as tk
 from tkinter import messagebox
 
 
 
 class cube(object):
-    row = 20
+    rows = 20
     w = 500
     def __init__(self, start, dirnx = 1, dirny = 0, colour = (255,0,0)):
-        pass
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.colour = colour
+
     def move(self, dirnx, dirny):
-        pass
-    def draw(self, surface, eyes = False):
-        pass
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+   
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.colour, (i*dis+1,j*dis+1, dis-2, dis-2))
+        if eyes: # Draws the eyes
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)   
 
 class snake(object):
     body = []
@@ -26,8 +44,9 @@ class snake(object):
 
     def move(self):
         for event in pygame.event.get():
-            if event in pygame.event.Quit:
+            if event.type == pygame.QUIT:
                 pygame.quit()
+                
             keys = pygame.key.get_pressed()
 
             for key in keys:
@@ -47,28 +66,28 @@ class snake(object):
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-        for i, c in enumerate(self.body): #i is the index c is cube object
+        for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
                 turn = self.turns[p]
-                c.move(turn[0], turn[1])
+                c.move(turn[0],turn[1])
                 if i == len(self.body)-1:
                     self.turns.pop(p)
             else:
-                if c.dirnx == -1 and c.pos[0] <= 0: (c.rows-1,c.pos[1])
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
                 elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0],0)
-                elif c.dirny == -1 and c.pos[1] <= 0: (c.pos[0],c.rows-1)
-                else:
-                    c.move(c.dirnx,c.dirny)
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
 
     def reset(self, pos):
         pass
     def add_cube(self):
         pass
+  
     def draw(self, surface):
         for i, c in enumerate(self.body):
-            if i == 0:
+            if i ==0:
                 c.draw(surface, True)
             else:
                 c.draw(surface)
@@ -86,8 +105,8 @@ def draw_grid(w, rows , surface):
 
 def redraw_window(surface):
     global rows, width, s
-    s.draw(surface)
     surface.fill((0,0,0))
+    s.draw(surface)
     draw_grid(width, rows, surface)
     pygame.display.update()
 
@@ -99,23 +118,18 @@ def message_box(subject, content):
     pass
 
 def main():
-    global width, rows , s
-    width = 500 #width of the screen
-    height = 500
-    rows = 20 #amount of rows
-    win = pygame.display.set_mode((width, height))
+    global width, rows, s
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
-    clock = pygame.time.Clock()
     flag = True
+
+    clock = pygame.time.Clock()
+    
     while flag:
         pygame.time.delay(50)
         clock.tick(10)
+        s.move()
         redraw_window(win)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                flag = False
-                pygame.quit()
-                sys.exit()  
-cube.rows = rows
-cube.w = w
 main()
