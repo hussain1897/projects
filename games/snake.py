7ae439d6c0b273b0dad5d20ac2bb474db04a4dcf
hussain1897 <ali-hussain10t@hotmail.co.uk>
2022-05-04 16:14:18 +0100

    finished game

--- games/snake.py
+++ games/snake.py
@@ -1,3 +1,4 @@
+from logging import root
 import pygame, random
 import math
 import tkinter as tk
@@ -82,7 +83,14 @@ class snake(object):
                 else: c.move(c.dirnx,c.dirny)
 
     def reset(self, pos):
-        pass
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    
     def addCube(self):
         tail = self.body[-1]
         dx, dy = tail.dirnx, tail.dirny
@@ -143,7 +151,16 @@ def random_snack(rows, item):
     return (x,y)
 
 def message_box(subject, content):
-    pass
+    root = tk.Tk()
+    root.attributes('-topmost', True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
+        
+    
 
 def main():
     global width, rows, s, snack
@@ -163,5 +180,12 @@ def main():
         if s.body[0].pos == snack.pos:
             s.addCube()
             snack = cube(random_snack(rows,s), colour = (0,255,0))
+        for x in range(len(s.body)):
+            if s.body[x].pos in list(map(lambda z:z.pos, s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You lost!', 'Play again...')
+                s.reset((10,10))
+                break
+
         redraw_window(win)
 main()
