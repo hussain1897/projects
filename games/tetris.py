c074058adcd8617de6af812ddddeb47085b7c85d
hussain1897 <ali-hussain10t@hotmail.co.uk>
2022-05-10 16:05:45 +0100

    initial shapes created

---
+++ games/tetris.py
@@ -0,0 +1,170 @@
+import pygame
+import random
+
+pygame.font.init()
+
+#global variables
+S_WIDTH = 800
+S_HEIGHT = 700
+PLAY_WIDTH = 300
+PLAY_HEIGHT = 600
+BLOCK_SIZE = 30
+
+TOP_LEFT_X = (S_WIDTH - PLAY_WIDTH) //2
+TOP_LEFT_Y = (S_HEIGHT - PLAY_HEIGHT)
+
+#SHAPE FORMATS 
+# SHAPE FORMATS
+
+S = [['.....',
+      '......',
+      '..00..',
+      '.00...',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colours = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.colour = shape_colours[shapes.index(shape)]
+        self.rotation = 0
+
+def create_grid(locked_positions={}):
+    pass
+
+def convert_shape_format(shape):
+    pass
+
+def valid_space(shape, grid):
+    pass
+
+def check_lost(positions):
+    pass
+
+def get_shape():
+    pass
+
+def draw_text_middle(text, size, colour, surface):  
+    pass
+   
+def draw_grid(surface, row, col):
+    pass
+
+def clear_rows(grid, locked):
+    pass
+
+def draw_next_shape(shape, surface):
+    pass
+
+def draw_window(surface):
+    pass
+
+def main():
+    pass
+
+def main_menu():
+    pass
+
+main_menu()  # start game
